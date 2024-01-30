220781321d96197c431ed03efda3893f54f4df1f
MaiDat <maidat.4work@gmail.com>
2024-01-30 14:54:42 +0700

    1

---
+++ src/test/java/da2/g35/tdtuconsultantbe/TdtuConsultantBeApplicationTests.java
@@ -0,0 +1,13 @@
+package da2.g35.tdtuconsultantbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdtuConsultantBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
